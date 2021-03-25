4e622416a2169cc04501b5ccf587d341d1711b5e
tony dugué <duguetony@gmail.com>
2021-03-25 11:40:45 +0100

    Initialisation du client

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Je suis le composant App !!!</h1>
+    </div>
+  );
+}
+
+export default App;
+
